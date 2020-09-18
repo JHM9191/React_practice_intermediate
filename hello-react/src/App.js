65ce7836d0c27c8a5db1e6a1b060ab841c6e839b
JHM9191 <hm.jo.9191@gmail.com>
2020-09-18 16:51:03 +0900

    using useState function in function type components

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 // import MyComponent from './MyComponent'
-import Counter from './Counter'
+// import Counter from './Counter'
+import Say from './Say'
 
 const App = () => {
   // return <MyComponent favoriteNumber={1}>리액트</MyComponent>
-  return <Counter />;
+  // return <Counter />
+  return <Say />
 }
 
 export default App;
