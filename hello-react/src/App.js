7700aa977544b2a56aed6fab7d4d92e08aebcfdc
JHM9191 <hm.jo.9191@gmail.com>
2020-09-18 15:40:13 +0900

    using state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
-import MyComponent from './MyComponent'
+// import MyComponent from './MyComponent'
+import Counter from './Counter'
 
 const App = () => {
-  return <MyComponent favoriteNumber={1}>리액트</MyComponent>
+  // return <MyComponent favoriteNumber={1}>리액트</MyComponent>
+  return <Counter />;
 }
 
 export default App;
