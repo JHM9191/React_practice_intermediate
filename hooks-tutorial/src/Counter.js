84570ac01d37bff730aaeb65516b59aab2d270ab
JHM9191 <hm.jo.9191@gmail.com>
2020-09-21 16:07:02 +0900

    worked on using useReducer()

--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+    switch(action.type) {
+        case 'INCREMENT':
+            return { value: state.value + 1 };
+        case 'DECREMENT':
+            return { value: state.value - 1 };
+        case 'X1000':
+            return { value: state.value * 1000};
+        case '/1000':
+            return { value: state.value / 1000};
+        default:
+            return state;
+    }
+}
 
 const Counter = () => {
-    const [value, setValue] = useState(0);
+    const [state, dispatch] = useReducer(reducer, { value:0 });
     return (
         <div>
-            <p>현재 카운터 값은 <b>{value}</b>입니다.</p>
-            <button onClick={() => setValue(value + 1)}>+1</button>
-            <button onClick={() => setValue(value - 1)}>-1</button>
+            <p>
+                현재 카운터 값은 <b>{state.value}</b>입니다.
+            </p>
+            <button onClick={() => dispatch({ type: 'INCREMENT' })}>+1</button>
+            <button onClick={() => dispatch({ type: 'DECREMENT' })}>-1</button>
+            <button onClick={() => dispatch({ type: 'X1000' })}>x 1000</button>
+            <button onClick={() => dispatch({ type: '/1000' })}>/ 1000</button>
         </div>
     );
-};
+}
 
 export default Counter;
