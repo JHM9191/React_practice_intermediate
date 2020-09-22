2775eb6d4f8cf07fee7d71972c2c3bd53fb6d832
JHM9191 <hm.jo.9191@gmail.com>
2020-09-22 16:58:11 +0900

    worked on API example: using local data before using API data

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1',);
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+  // const [data, setData] = useState(null);
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=d16d55112de249f9aef6a162ff89d709',);
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
 
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} cols={50} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <div>
+  //       <button onClick={onClick}>불러오기</button>
+  //     </div>
+  //     {data && <textarea rows={20} cols={50} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+  //   </div>
+  // );
+  return <NewsList />
 };
 
 export default App;
