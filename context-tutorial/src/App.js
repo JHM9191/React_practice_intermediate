31dfbeeb163f03ca6be27cea6cb312032279c880
JHM9191 <hm.jo.9191@gmail.com>
2020-09-22 21:29:55 +0900

    worked on context provider

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import ColorContext from './contexts/color';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: 'red' }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 }
-
 export default App;
